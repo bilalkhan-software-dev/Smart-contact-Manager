1934066b772edb769ed77cc7725c0d5a85d532d0
bilalkhan-software-dev <bilalkhan.devse@gmail.com>
2025-04-28 02:58:12 +0500

    Exact Java 21 fix

--- .railway/install.sh
+++ .railway/install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# Install Amazon Corretto 21 (verified working JDK)
-apt-get update && apt-get install -y wget
-wget https://corretto.aws/downloads/latest/amazon-corretto-21-x64-linux-jdk.deb
-dpkg -i amazon-corretto-21-x64-linux-jdk.deb
+# Force install Corretto JDK 21
+apt-get update
+apt-get install -y wget
+wget -O corretto.deb https://corretto.aws/downloads/latest/amazon-corretto-21-x64-linux-jdk.deb
+dpkg -i corretto.deb
+rm corretto.deb
 
 # Verify installation
 /usr/lib/jvm/amazon-corretto-21-jdk-amd64/bin/java -version
